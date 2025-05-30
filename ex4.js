e5ca95dbb870559bbb30eb1c581deaf7028c2507
Rebeca <bekilds@gmail.com>
2025-05-30 16:11:18 -0300

    um

---
+++ ex4.js
@@ -0,0 +1,7 @@
+function inverteString(str){
+return str.split('').reverse().join('')
+}
+let stringOriginal = "thiago"
+let stringInvertida = inverteString
+console.log("String original",stringOriginal)
+console.log("string invertida",stringInvertida)
