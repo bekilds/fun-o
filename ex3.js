7f646c1d6328a69418585e86de606e6ac0b0fcc3
Rebeca <bekilds@gmail.com>
2025-05-30 15:44:51 -0300

    um

---
+++ ex3.js
@@ -0,0 +1,14 @@
+function MaioresDeTres(a, b, c){
+    if(a >= b && a >= b){
+        return  a;
+} else if(b >= a && b >= c){
+return b;
+} else {
+ return c;  
+}
+}
+console.log(MaioresDeTres(5, 9, 2));
+console.log(MaioresDeTres(-3, -7, -1))
+
+
+
